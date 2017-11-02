01ec8b0abd6c20737e13a305d5d70260d0d42952
Ocean_hy <he88012512@163.com>
2017-11-02 15:02:54 +0800

    eam init

---
+++ src/main/java/com/wp/demo/BjeamApplication.java
@@ -0,0 +1,12 @@
+package com.wp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjeamApplication.class, args);
+	}
+}
