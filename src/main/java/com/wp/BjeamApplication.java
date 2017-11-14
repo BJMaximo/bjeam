8f00684b02cdc8389b44a94f00665b0b74199a79
Ocean_hy <he88012512@163.com>
2017-11-14 14:37:27 +0800

    Update Start Application Package

--- src/main/java/com/wp/BjeamApplication.java
+++ src/main/java/com/wp/BjeamApplication.java
@@ -1,9 +1,11 @@
-package com.wp.demo;
+package com.wp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.wp.**"})
 public class BjeamApplication {
 
 	public static void main(String[] args) {
