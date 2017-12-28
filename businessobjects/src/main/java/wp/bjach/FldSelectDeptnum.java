7aa5089bf670b12590cf658ab07efbb9434e5434
lupe <zingpan@foxmail.com>
2017-12-28 18:39:52 +0800

    test class

---
+++ businessobjects/src/main/java/wp/bjach/FldSelectDeptnum.java
@@ -0,0 +1,48 @@
+package wp.bjach;
+
+import psdi.mbo.MAXTableDomain;
+import psdi.mbo.MboRemote;
+import psdi.mbo.MboSetRemote;
+import psdi.mbo.MboValue;
+import psdi.util.MXException;
+
+import java.rmi.RemoteException;
+
+/**
+ * ╔════════════════════════════════╗
+ * §File Name:  FldSelectDeptnum.java
+ * §File Path: wp.bjach.FldSelectDeptnum
+ * §Descrption: 绩效管理-选择部门
+ * §Version:  V0.1
+ * §Create Date:   2017/12/28
+ * §IDE:    IntelliJ IDEA.2017
+ * §Font Code:  UTF-8
+ * §JDK :1.6
+ * §Author: lupe
+ * §History Version Note:
+ * ╚════════════════════════════════╝
+ */
+public class FldSelectDeptnum extends MAXTableDomain {
+    public FldSelectDeptnum(MboValue mbv) {
+        super(mbv);
+        setRelationship("BJDEPT", "");//数据来源表的名称
+        String[] strFrom = {"BJDEPTNUM"};//数据来源字段
+        String[] strTo = {getMboValue().getAttributeName()};//目标表字段
+        setLookupKeyMapInOrder(strTo, strFrom);
+    }
+
+    @Override
+    public MboSetRemote getList() throws MXException, RemoteException {
+        MboRemote thisMbo=getMboValue().getMbo();
+        setListCriteria("orgid='"+thisMbo.getString("ORGID")+"'");
+        return super.getList();
+    }
+
+    @Override
+    public void validate() throws MXException, RemoteException {
+    }
+
+    @Override
+    public void action() throws MXException, RemoteException {
+    }
+}
