d525bf9cafde4b471251851c6f1f2e0bd13e46f6
lupe <zingpan@foxmail.com>
2018-01-04 15:18:21 +0800

    test class

--- businessobjects/src/main/java/wp/bjcontract/FldJGZCSelectStatus.java
+++ businessobjects/src/main/java/wp/bjcontract/FldJGZCSelectStatus.java
@@ -1,9 +1,6 @@
 package wp.bjcontract;
 
-import psdi.mbo.MAXTableDomain;
-import psdi.mbo.MboRemote;
-import psdi.mbo.MboSetRemote;
-import psdi.mbo.MboValue;
+import psdi.mbo.*;
 import psdi.util.MXException;
 
 import java.rmi.RemoteException;
@@ -33,8 +30,6 @@ public class FldJGZCSelectStatus extends MAXTableDomain {
 
     @Override
     public MboSetRemote getList() throws MXException, RemoteException {
-        MboRemote thisMbo=getMboValue().getMbo();
-//        setListCriteria("orgid='"+thisMbo.getString("ORGID")+"'");
         return super.getList();
     }
 
@@ -44,5 +39,47 @@ public class FldJGZCSelectStatus extends MAXTableDomain {
 
     @Override
     public void action() throws MXException, RemoteException {
+        MboRemote thisMbo=getMboValue().getMbo();
+        if(thisMbo!=null){
+            String jgStatus=thisMbo.getString("JGSTATUS");
+            Translate translate = getTranslator();
+            String trStatus=translate.toInternalString("JGSTATUS",jgStatus);
+            if("现场施工".equalsIgnoreCase(trStatus) || "项目验收".equalsIgnoreCase(trStatus)){
+                thisMbo.setFieldFlag("BJVENDORNUM",128L,true);
+            }else if("取消暂停".equalsIgnoreCase(trStatus)){
+                setFieldOnlyReadOrOther(thisMbo,7L,true);//设置界面所有字段为只读
+            }else{
+                thisMbo.setFieldFlag("BJVENDORNUM",128L,false);
+                setFieldOnlyReadOrOther(thisMbo,7L,false);//设置界面所有字段不为只读
+            }
+            thisMbo.setValue("BJTRANSLATE",trStatus);//设置同义词内部值，用于保存时判断逻辑
+        }
     }
+
+    /**
+     * 为当前界面所有字段设置状态
+     * @param mbo mbo
+     * @param statusCode 状态代码 只读为7L，必填为128L
+     * @param flag true或false
+     * @throws RemoteException
+     */
+    public void setFieldOnlyReadOrOther(MboRemote mbo,long statusCode,boolean flag) throws RemoteException {
+        mbo.setFieldFlag("PROJECTTYPE",statusCode,flag);//项目类别
+        mbo.setFieldFlag("AMOUNT",statusCode,flag);//项目总投资
+        mbo.setFieldFlag("SERVICENAME",statusCode,flag);//项目名称
+        mbo.setFieldFlag("SERVICECONTENT",statusCode,flag);//具体内容
+        mbo.setFieldFlag("DESRANGE",statusCode,flag);//立项原因
+        mbo.setFieldFlag("LEAD",statusCode,flag);//负责人
+        mbo.setFieldFlag("BJDEPTNUM",statusCode,flag);//部门
+        mbo.setFieldFlag("ATTRIBUTE",statusCode,flag);//属性
+        mbo.setFieldFlag("PROGRESS",statusCode,flag);//进度%
+        mbo.setFieldFlag("REGION",statusCode,flag);//区域
+        mbo.setFieldFlag("LOCATION",statusCode,flag);//实施区域
+        mbo.setFieldFlag("BJVENDORNUM",statusCode,flag);//定点供应商
+        mbo.setFieldFlag("STARTTIME",statusCode,flag);//开始时间
+        mbo.setFieldFlag("ENDTIME",statusCode,flag);//结束时间
+        mbo.setFieldFlag("REMARKS",statusCode,flag);//备注
+
+    }
+
 }
