fc8127192ea8459f5643a7420396cd7a2c819e7d
lupe <zingpan@foxmail.com>
2018-01-11 15:09:47 +0800

    test class

---
+++ businessobjects/src/main/java/wp/bjinsstabook/FldSelectAssetnum.java
@@ -0,0 +1,48 @@
+package wp.bjinsstabook;
+
+import psdi.mbo.MAXTableDomain;
+import psdi.mbo.MboRemote;
+import psdi.mbo.MboSetRemote;
+import psdi.mbo.MboValue;
+import psdi.util.MXException;
+
+import java.rmi.RemoteException;
+
+/**
+ * ╔════════════════════════════════╗
+ * §File Name:  FldSelectLocation.java
+ * §File Path: wp.bjinsstabook.FldSelectAssetnum
+ * §Descrption: 点检标准应用程序--选择设备
+ * §Version:  V0.1
+ * §Create Date:   2018/1/4
+ * §IDE:    IntelliJ IDEA.2017
+ * §Font Code:  UTF-8
+ * §JDK :1.6
+ * §Author: lupe
+ * §History Version Note:
+ * ╚════════════════════════════════╝
+ */
+public class FldSelectAssetnum extends MAXTableDomain {
+    public FldSelectAssetnum(MboValue mbv) {
+        super(mbv);
+        setRelationship("ASSET", "");//数据来源表的名称
+        String[] strFrom = {"ASSETNUM"};//数据来源字段
+        String[] strTo = {getMboValue().getAttributeName()};//目标表字段
+        setLookupKeyMapInOrder(strTo, strFrom);
+    }
+
+    @Override
+    public MboSetRemote getList() throws MXException, RemoteException {
+        MboRemote thisMbo=getMboValue().getMbo();
+        setListCriteria("orgid='"+thisMbo.getString("orgid")+"'");
+        return super.getList();
+    }
+
+    @Override
+    public void validate() throws MXException, RemoteException {
+    }
+
+    @Override
+    public void action() throws MXException, RemoteException {
+    }
+}
---
+++ businessobjects/src/main/java/wp/bjinsstabook/FldSelectLocation.java
@@ -0,0 +1,49 @@
+package wp.bjinsstabook;
+
+import psdi.mbo.MAXTableDomain;
+import psdi.mbo.MboRemote;
+import psdi.mbo.MboSetRemote;
+import psdi.mbo.MboValue;
+import psdi.util.MXException;
+
+import java.rmi.RemoteException;
+
+/**
+ * ╔════════════════════════════════╗
+ * §File Name:  FldSelectLocation.java
+ * §File Path: wp.bjinsstabook.FldSelectLocation
+ * §Descrption: 点检标准书应用程序--选择区域
+ * §Version:  V0.1
+ * §Create Date:   2018/1/11
+ * §IDE:    IntelliJ IDEA.2017
+ * §Font Code:  UTF-8
+ * §JDK :1.6
+ * §Author: lupe
+ * §History Version Note:
+ * ╚════════════════════════════════╝
+ */
+public class FldSelectLocation extends MAXTableDomain {
+
+    public FldSelectLocation(MboValue mbv) {
+        super(mbv);
+        setRelationship("LOCATIONS", "");//数据来源表的名称
+        String[] strFrom = {"LOCATION"};//数据来源字段
+        String[] strTo = {getMboValue().getAttributeName()};//目标表字段
+        setLookupKeyMapInOrder(strTo, strFrom);
+    }
+
+    @Override
+    public MboSetRemote getList() throws MXException, RemoteException {
+        MboRemote thisMbo=getMboValue().getMbo();
+//        setListCriteria("orgid='"+thisMbo.getString("orgid")+"'");
+        return super.getList();
+    }
+
+    @Override
+    public void validate() throws MXException, RemoteException {
+    }
+
+    @Override
+    public void action() throws MXException, RemoteException {
+    }
+}
